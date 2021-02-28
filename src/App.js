9e15db5a0e6e4e6bb914c2986017d448586bd3de
rakibadeffi <rakib@adeffi.com>
2021-02-28 09:37:31 +0600

    api integreted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const[posts,setPosts] = useState({});
+    useEffect(()=> {
+      fetch("https://xkcd.com/info.0.json", {
+        mode: 'no-cors',
+  method: 'get',
+  headers: {  
+    "Content-Type": "application/json",
+    "Accept":"*/*",
+    
+  },
+})   
+            .then(res => res.json())
+            .then(data=> setPosts(data))
+    }, []);
+  
+    const {month, num, link, year,news,safe_title,transcript,alt,img,title,day } = posts;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
 
 export default App;
+
